59684a164f99bd2f82602975ebc655bbaedf4842
roblanf <rob.lanfear@gmail.com>
2017-10-05 10:28:46 +1100

    update script
    a bunch of changes: first we take input and output arguments. Also we
    have a little more output for users.

--- py_script/SWSCEN.py
+++ py_script/SWSCEN.py
@@ -2,19 +2,26 @@ from utilities import *
 from functions_metrics import *
 import os
 from time import asctime
+import sys
 
 
-datasets = ['/Volumes/VATagliacollo/GitHub/PFinderUCE-SWSC-EN/example/example_dataset.nex']
+dataset_path = sys.argv[1]
+try:
+	output_path = sys.argv[2]
+except:
+	output_path = os.path.dirname(dataset_path)
 
+print ("\n")
+print ("analysing %s", dataset_path)
 
-for dataset_path in datasets:
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
 
-	print ("\n")
-	print (dataset_path)
+os.chdir(output_path)
 
-	os.chdir(output_paths(dataset_path))
+name = os.path.basename(dataset_path).rstrip(".nex")
 
-	name = os.path.basename(dataset_path).rstrip(".nex")
+print(asctime())
+process_dataset_metrics(dataset_path, ['entropy'], minimum_window_size = 50, outfilename = '%s.csv' % (name))
 
-	print(asctime())
-	process_dataset_metrics(dataset_path, ['entropy'], minimum_window_size = 50, outfilename = '%s.csv' % (name))
+print ("Done. Output is here %s", output_path)
